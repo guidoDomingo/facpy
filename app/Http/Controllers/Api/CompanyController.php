c6627ba744b8ce67289e4874c7c7dfe8db191181
Victor Arana <victor@codersfree.com>
2023-08-09 18:47:54 -0500

    Crear empresas

---
+++ app/Http/Controllers/Api/CompanyController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return auth()->user();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'razon_social' => 'required|string|max:255',
+            'ruc' => 'required|string|max:11',
+            'direccion' => 'required|string|max:255',
+            'logo' => 'nullable|file|image',
+            'sol_user' => 'required|string|max:255',
+            'sol_pass' => 'required|string|max:255',
+            'cert' => 'required|file|mimes:pem,txt',
+            'client_id' => 'nullable|string|max:255',
+            'client_secret' => 'nullable|string|max:255',
+            'production' => 'nullable|boolean',
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $data['logo_path'] = $request->file('logo')->store('logos');
+        }
+
+        $data['cert_path'] = $request->file('cert')->store('certs');
+        $data['user_id'] = auth()->id();
+
+        $company = Company::create($data);
+
+        return response()->json([
+            'message' => 'Empresa creada correctamente',
+            'company' => $company,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Company $company)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Models\Company;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('register', [RegisterController::class, 'store']);
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+Route::post('me', [AuthController::class, 'me']);
+
+Route::apiResource('companies', CompanyController::class)
+    ->middleware('auth:api');
